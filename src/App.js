3cd85574017220c7719dc5b4aa79bf78851d7bb7
zhangweijian <zhangweijian@mchz.com.cn>
2021-09-09 19:41:40 +0800

    第一次绑定个人项目到github🐂

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Component from './components/practice01'
+function App() {
+  return (
+    <div className="App">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/practice01.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function practice01() {
+    return (
+        <div>
+            practice01
+        </div>
+    )
+}
